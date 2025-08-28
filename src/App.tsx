e8aa13a157fa9c6588e81a1204d58445b5b5679a
mateus <mateusweslley14@gmail.com>
2025-08-28 15:11:45 -0300

    refactor: simplify App component and migrate logic to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,9 @@
-import { useState, useCallback } from "react";
-import FormSection from "./components/FormSection.jsx";
-import ResumePreview from "./components/ResumePreview.jsx";
+import { Home } from "./pages/Home.js";
 
 export default function App() {
-  const [resumeData, setResumeData] = useState({
-    personalInfo: {
-      name: "",
-      email: "",
-      phone: "",
-      linkedin: "",
-      summary: "",
-    },
-    education: [],
-    experience: [],
-    skills: [],
-  });
-
-  const handlePersonalInfoChange = useCallback((e) => {
-    const { name, value } = e.target;
-    setResumeData((prevData) => ({
-      ...prevData,
-      personalInfo: {
-        ...prevData.personalInfo,
-        [name]: value,
-      },
-    }));
-  }, []);
-
   return (
-    <div className="bg-gray-50 min-h-screen p-4 md:p-8 flex justify-center items-center font-sans">
-      <div className="w-full max-w-7xl h-[90vh] flex flex-col md:flex-row bg-white rounded-3xl shadow-xl overflow-hidden">
-        {}
-        <FormSection
-          data={resumeData}
-          onDataChange={handlePersonalInfoChange}
-        />
-        <ResumePreview data={resumeData} />
-      </div>
-    </div>
-  );
+    <>
+      <Home />
+    </>
+  )
 }
