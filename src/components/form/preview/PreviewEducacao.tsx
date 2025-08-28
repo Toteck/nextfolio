6b23cc658845c71a3f97618c8a1b9007a5e8577c
mateus <mateusweslley14@gmail.com>
2025-08-28 17:14:11 -0300

    feat: add component to preview education

---
+++ src/components/form/preview/PreviewEducacao.tsx
@@ -0,0 +1,23 @@
+// Exibir todas as formações acadêmicas do usuário
+
+export function PreviewEducacao() {
+    return (
+        <div>
+            <h2 className="text-xl font-bold mb-4">Educação</h2>
+            {/*
+              Aqui as formações acadêmicas do usuário serão exibidas como:
+              
+              Instituição: Universidade X
+              Curso: Bacharelado em Ciência da Computação
+              Período: 2018 - 2022
+              Status: Concluído
+              
+              ---
+              Instituição: Escola Y
+              Curso: Ensino Médio
+              Período: 2015 - 2017
+              Status: Finalizado
+            */}
+        </div>
+    )
+}
