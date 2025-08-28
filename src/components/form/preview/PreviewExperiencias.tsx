6ec281f2e4787bca11b6c2be2a216ed4406407f5
mateus <mateusweslley14@gmail.com>
2025-08-28 14:53:34 -0300

    feat: add initial structure for experience preview

---
+++ src/components/form/preview/PreviewExperiencias.tsx
@@ -0,0 +1,15 @@
+// Exibir todas as experiencias do usuario
+
+export function PreviewExperiencias() {
+    return (
+        <div>
+            <h2 className="text-xl font-bold mb-4">Experiências</h2>
+            {/*Aqui as experiencias do usuário serão exibidas como:
+            Empresa X
+            Cargo Y
+            Período: 2020 - 2022
+            Descrição: Atividades realizadas na empresa X como cargo Y.
+        */}
+        </div>
+    )
+}
