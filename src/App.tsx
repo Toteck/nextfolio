fb91f3e6648e1d30281e32d91ba303c25c636188
Stephan Lessa <stephanamlessa@gmail.com>
2025-08-31 19:31:26 -0300

    fix/corrigir app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Home } from "./pages/Home.js";
+import { ResumeProvider } from "./curriculoContext";
 
 export default function App() {
   return (
-    <>
+    <ResumeProvider>
       <Home />
-    </>
-  )
+    </ResumeProvider>
+  );
 }
