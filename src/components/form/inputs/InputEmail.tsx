c744f7edb99d24dcc4c750e9352e64bacc68b4eb
Stephan Lessa <stephanamlessa@gmail.com>
2025-09-01 00:45:36 -0300

    feat: adicionar input e-mail

--- src/components/form/inputs/InputEmail.tsx
+++ src/components/form/inputs/InputEmail.tsx
@@ -1,5 +1,18 @@
-export function InputEmail() {
-    return (
-        <></>
-    )
+interface InputEmailProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function InputEmail({ label, value, onChange }: InputEmailProps) {
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        type="email"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
 }
