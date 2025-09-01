ce12befe27bce5ce42f2b51391303651342cf819
Stephan Lessa <stephanamlessa@gmail.com>
2025-09-01 00:43:46 -0300

    feat: input resumo profissional

--- src/components/form/inputs/TextAreaResumo.tsx
+++ src/components/form/inputs/TextAreaResumo.tsx
@@ -1,5 +1,27 @@
-export const TextAreaResumo = () => {
-    return (
-        <></>
-    )
+interface TextAreaResumoProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  maxLength?: number;
+}
+
+export function TextAreaResumo({
+  label,
+  value,
+  onChange,
+  maxLength = 500,
+}: 
+TextAreaResumoProps) {
+  return (
+    <div>
+      <label>{label}</label>
+      <textarea
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        maxLength={maxLength}
+        rows={4}
+      />
+      <span>{value.length}/{maxLength} caracteres</span>
+    </div>
+  );
 }
