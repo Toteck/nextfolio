bc9449d93b005aa52a35d34a997d029fdfe2099c
Stephan Lessa <stephanamlessa@gmail.com>
2025-09-01 00:47:07 -0300

    feat: adicionar input linkedin

--- src/components/form/inputs/InputLinkedIn.tsx
+++ src/components/form/inputs/InputLinkedIn.tsx
@@ -1,5 +1,19 @@
-export function InputLinkedIn() {
-    return (
-        <></>
-    )
+interface InputLinkedInProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function InputLinkedIn({ label, value, onChange }: InputLinkedInProps) {
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        type="url"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="https://linkedin.com/in/seu-perfil"
+      />
+    </div>
+  );
 }
