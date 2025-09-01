97a8f4b268e1464be34b93365a35220498b5a3ec
Stephan Lessa <stephanamlessa@gmail.com>
2025-09-01 00:46:39 -0300

    feat: adicionar input nome e nome social

--- src/components/form/inputs/InputText.tsx
+++ src/components/form/inputs/InputText.tsx
@@ -1,5 +1,18 @@
-export function InputText() {
-    return (
-        <></>
-    )
+interface InputTextProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function InputText({ label, value, onChange }: InputTextProps) {
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
 }
