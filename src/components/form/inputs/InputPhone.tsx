534f952808fb790b341ad674dfcad9ebd7a11994
Stephan Lessa <stephanamlessa@gmail.com>
2025-09-01 00:46:14 -0300

    feat: adicionar input telefone

--- src/components/form/inputs/InputPhone.tsx
+++ src/components/form/inputs/InputPhone.tsx
@@ -1,5 +1,19 @@
-export function InputPhone() {
-    return (
-        <></>
-    )
+interface InputPhoneProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function InputPhone({ label, value, onChange }: InputPhoneProps) {
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        type="tel"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="(99) 99999-9999"
+      />
+    </div>
+  );
 }
