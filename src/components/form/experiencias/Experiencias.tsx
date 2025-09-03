11d16903c75ef1bcbee54724f87388006c75cb16
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-09-03 20:58:41 +0000

    refactor: add functions to add, change and remove itens

--- src/components/form/experiencias/Experiencias.tsx
+++ src/components/form/experiencias/Experiencias.tsx
@@ -1,18 +1,49 @@
-import { InputCargo } from "./InputCargo";
-import { InputEmpresa } from "./InputEmpresa";
-import { InputPeriodo } from "./InputPeriodo";
-import { TextAreaDescricao } from "./TextAreaDescricao";
+import { useResume, type Experiencia } from "../../../context/CurriculoContext";
+import { Button } from "../../Button";
+import { ExperienciaItem } from "./ExperienciaItem";
+import { v4 as uuidv4 } from "uuid";
 
 export function Experiencias() {
+  const { experiencia, setExperiencia } = useResume();
+
+  const handleAdd = () => {
+    const id = uuidv4()
+
+    setExperiencia((prevExperiencia) => [
+      ...prevExperiencia,
+      {
+        id,
+        cargo: "",
+        empresa: "",
+        anoInicio: "",
+        anoFim: "",
+        descricaoAtividades: ""
+      }
+    ])
+  }
+
+  const handleRemove = (id: string) => {
+    setExperiencia((prevExperiencia) => prevExperiencia.filter((item) => item.id !== id))
+  }
+
+
+  const handleChange = (id: string, field: keyof Experiencia, value: string) => {
+    setExperiencia((prevExperiencia) =>
+      prevExperiencia.map((item) =>
+        item.id === id ? { ...item, [field]: value } : item
+      )
+    );
+  };
+
+
   return (
     <div>
       <h2 className="text-xl font-bold mb-4">Experiências</h2>
-      <div className="space-y-4">
-        <InputCargo />
-        <InputEmpresa />
-        <InputPeriodo />
-        <TextAreaDescricao />
-      </div>
+      {experiencia.map((item) => (
+        <ExperienciaItem key={item.id} item={item} onChange={(field, value) => handleChange(item.id, field, value)}
+          onRemove={() => handleRemove(item.id)} />
+      ))}
+      <Button label="Adicionar Experiência" onClick={handleAdd} />
     </div>
   );
 }
