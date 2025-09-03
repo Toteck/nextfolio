544e51045ba194d9067f1488933f9fef4f933d64
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-09-03 20:49:41 +0000

    feat: experiencia item to render experiencies

--- src/components/form/experiencias/ExperienciaItem.tsx
+++ src/components/form/experiencias/ExperienciaItem.tsx
@@ -1,12 +1,19 @@
+import type { Experiencia } from "../../../context/CurriculoContext";
 import { InputCargo } from "./InputCargo";
 import { InputEmpresa } from "./InputEmpresa";
 import { InputPeriodo } from "./InputPeriodo";
 import { TextAreaDescricao } from "./TextAreaDescricao";
 
-export function ExperienciaItem() {
+type ExperienciaItemProps = {
+    item: Experiencia;
+    onChange: (field: keyof Experiencia, value: string) => void;
+    onRemove: () => void;
+}
+
+export function ExperienciaItem({item, onChange, onRemove}: ExperienciaItemProps) {
     return (
-        <div className="space-y-4">
-            <InputCargo />
+        <div className="p-4 border rounded-md space-y-4">
+            <InputCargo  />
             <InputEmpresa />
             <InputPeriodo />
             <TextAreaDescricao />
