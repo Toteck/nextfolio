5adf741cc7121e2453d856f3cd0ffcb0be9f4800
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-09-05 00:57:21 +0000

    refactor: implement atributes

--- src/components/form/experiencias/InputCargo.tsx
+++ src/components/form/experiencias/InputCargo.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+interface InputCargoProps {
+  value: string;
+  onChange: (value: string) => void;
+}
 
-export function InputCargo() {
-  const [cargo, setCargo] = useState("");
+export function InputCargo({ value, onChange }: InputCargoProps) {
+
   return (
     <div className="flex flex-col">
       <label className="font-medium mb-1">
@@ -10,8 +13,8 @@ export function InputCargo() {
       <input
         type="text"
         placeholder="Ex: Desenvolvedor Front-End"
-        value={cargo}
-        onChange={(e) => setCargo(e.target.value)}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
         className="border border-black rounded-md px-3 py-2 bg-white focus:outline-none focus:border-blue-500"
       />
     </div>
