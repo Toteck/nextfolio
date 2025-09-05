f1898c6048f21be386290bebf69bf7fec3c51da8
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-09-05 00:57:38 +0000

    refactor: add atributes

--- src/components/form/experiencias/InputEmpresa.tsx
+++ src/components/form/experiencias/InputEmpresa.tsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
 
-export function InputEmpresa() {
-  const [empresa, setEmpresa] = useState("");
+
+interface InputEmpresaProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function InputEmpresa({ value, onChange }: InputEmpresaProps) {
+
   return (
     <div className="flex flex-col">
       <label className="font-medium mb-1">
@@ -10,8 +15,8 @@ export function InputEmpresa() {
       <input
         type="text"
         placeholder="Ex: Avenue"
-        value={empresa}
-        onChange={(e) => setEmpresa(e.target.value)}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
         className="border border-black rounded-md px-3 py-2 bg-white focus:outline-none focus:border-blue-500"
       />
     </div>
