79e6f46b42aadeedafe0d84790a8275bb662b503
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-09-06 00:17:48 +0000

    refactor: reestructure component and add properties

--- src/components/form/experiencias/InputPeriodo.tsx
+++ src/components/form/experiencias/InputPeriodo.tsx
@@ -1,32 +1,26 @@
-import { useState } from "react";
+interface InputPeriodoProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+}
 
-export function InputPeriodo() {
-  const [startPeriod, setStartPeriod] = useState("");
-  const [finishPeriod, setFinishPeriod] = useState("");
+
+export function InputPeriodo({value, onChange, label}:InputPeriodoProps) {
+
   return (
     <div className="flex flex-col">
-      <label className="font-medium mb-1">
-        Período <span className="text-red-500">*</span>
-      </label>
+      
       <div className="flex items-center gap-4">
         <div className="flex flex-col">
-          <span className="text-sm">De</span>
+          <span className="text-md">{label}</span>
           <input
             type="date"
-            value={startPeriod}
-            onChange={(e) => setStartPeriod(e.target.value)}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
             className="border border-black rounded-md px-3 py-2 bg-white focus:outline-none focus:border-blue-500"
           />
         </div>
-        <div className="flex flex-col">
-          <span className="text-sm">Até</span>
-          <input
-            type="date"
-            value={finishPeriod}
-            onChange={(e) => setFinishPeriod(e.target.value)}
-            className="border border-black rounded-md px-3 py-2 bg-white focus:outline-none focus:border-blue-500"
-          />
-        </div>
+        
       </div>
     </div>
   );
