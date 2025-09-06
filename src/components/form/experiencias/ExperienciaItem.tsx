3ce15ec2ac7a35f137c703a80b418e594d8401db
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-09-06 00:19:24 +0000

    refactor: component restructuring

--- src/components/form/experiencias/ExperienciaItem.tsx
+++ src/components/form/experiencias/ExperienciaItem.tsx
@@ -11,14 +11,20 @@ type ExperienciaItemProps = {
     onRemove: () => void;
 }
 
-export function ExperienciaItem({item, onChange, onRemove}: ExperienciaItemProps) {
+export function ExperienciaItem({ item, onChange, onRemove }: ExperienciaItemProps) {
     return (
         <div className="p-4 border rounded-md space-y-4">
             <InputCargo value={item.cargo} onChange={(value) => onChange("cargo", value)} />
-            <InputEmpresa value={item.empresa} onChange={(value) => onChange("empresa", value)}/>
-            <InputPeriodo />
-            <TextAreaDescricao value={item.cargo} onChange={(value) => onChange("cargo", value)}/>
-            <Button label="Remover Experiência" onClick={() => alert("removendo experiência")} variant="remove"/>
+            <InputEmpresa value={item.empresa} onChange={(value) => onChange("empresa", value)} />
+            <p className="font-medium mb-1">
+                Período <span className="text-red-500">*</span>
+            </p>
+            <div className="flex  justify-between">
+                <InputPeriodo label="De" value={item.anoInicio} onChange={(value) => onChange("anoInicio", value)} />
+                <InputPeriodo label="Até" value={item.anoFim} onChange={(value) => onChange("anoFim", value)} />
+            </div>
+            <TextAreaDescricao value={item.descricaoAtividades} onChange={(value) => onChange("descricaoAtividades", value)} />
+            <Button label="Remover Experiência" onClick={onRemove} variant="remove" />
         </div>
     )
 }
