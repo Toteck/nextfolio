3aaf6f12c56ed8846588b5d5c0e3eb84e5dc052b
mateus <mateusweslley14@gmail.com>
2025-09-08 17:04:25 -0300

    refactor: Update HabilidadeItem component to use item prop and improve structure

--- src/components/form/habilidades/HabilidadeItem.tsx
+++ src/components/form/habilidades/HabilidadeItem.tsx
@@ -1,19 +1,23 @@
 import { FaTrash } from "react-icons/fa";
 import { Button } from "../../Button";
+import type { Habilidade } from "../../../context/CurriculoContext";
+import { InputHabilidades } from "./InputHabilidade";
+import { SelectNivel } from "./SelectNivel";
 
 type HabilidadeItemProps = {
-    nome: string;
-    nivel: "Iniciante" | "Intermediário" | "Avançado";
+    item: Habilidade;
+    onChange: (field: keyof Habilidade, value: string) => void;
     onRemove: () => void;
 }
 
-export function HabilidadeItem({ nome, nivel, onRemove }: HabilidadeItemProps) {
+export function HabilidadeItem({ item, onRemove, onChange }: HabilidadeItemProps) {
     return (
-        <div className="flex items-center justify-between p-4 bg-gray-50 rounded-md border border-gray-200">
-            <div className="flex-1">
-                <p className="text-gray-800 font-semibold">{nome}</p>
-                <p className="text-gray-500 text-sm italic">{nivel}</p>
+        <div className="flex items-center justify-between p-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <InputHabilidades value={item.nome} onChange={(value) => onChange("nome", value)} />
+                <SelectNivel value={item.nivel} onChange={(value) => onChange("nivel", value)} />
             </div>
+
             <Button
                 onClick={onRemove}
                 variant="remove"
