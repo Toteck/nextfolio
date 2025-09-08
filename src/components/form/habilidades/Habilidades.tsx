125832f03194aec80c7fa7a007e8c4e653326e1a
mateus <mateusweslley14@gmail.com>
2025-09-08 17:06:51 -0300

    feat: Integrate useResume context for managing habilidades and enhance component structure

--- src/components/form/habilidades/Habilidades.tsx
+++ src/components/form/habilidades/Habilidades.tsx
@@ -1,30 +1,51 @@
-import { habilidades } from "../../../mockup/habilidades";
+import { useResume, type Habilidade } from "../../../context/CurriculoContext";
 import { Button } from "../../Button";
 import { HabilidadeItem } from "./HabilidadeItem";
-import { InputHabilidades } from "./InputHabilidade";
-import { SelectNivel } from "./SelectNivel";
 import { FaPlus } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid"
 
 export function Habilidades() {
+    const { habilidades, setHabilidades } = useResume();
+
+
+
+    const handleRemove = (id: string) => {
+        setHabilidades((prevHabilidades) => prevHabilidades.filter((item) => item.id !== id))
+    }
+
+    const handleChange = (id: string, field: keyof Habilidade, value: string) => {
+        setHabilidades((prevHabilidades) =>
+            prevHabilidades.map((item) =>
+                item.id === id ? { ...item, [field]: value } : item
+            )
+        );
+    };
+
+    const handleAdd = () => {
+        const id = uuidv4()
+
+        setHabilidades((prevHabilidades) => [
+            ...prevHabilidades,
+            {
+                id,
+                nome: "",
+                nivel: "Iniciante"
+            }
+        ])
+    }
+
     return (
         <div>
             <h2 className="text-xl font-bold mb-4">Habilidades</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <InputHabilidades />
-                <SelectNivel />
-            </div>
-
-
-            <div className="flex">
-                <Button label="Adicionar Habilidade" icon={<FaPlus />} tooltip="Adicionar Habilidade" onClick={() => { alert("Adicionando habilidade") }} />
-            </div>
-
             {/* Lista de habilidades adicionadas */}
             <div className="mt-6 space-y-4">
                 {/* Exemplo de habilidade */}
-                {habilidades.length > 0 ? (habilidades.map((skill, index) => <HabilidadeItem key={index} nome={skill.nome} onRemove={() => { }} nivel={skill.nivel} />)) : (<p className="text-gray-600 italic mb-4">Nenhuma habilidade adicionada ainda.</p>)}
+                {habilidades.length > 0 ? (habilidades.map((skill) => <HabilidadeItem key={skill.id} item={skill} onRemove={() => handleRemove(skill.id)} onChange={(field, value) => handleChange(skill.id, field, value)} />)) : (<p className="text-gray-600 italic mb-4">Nenhuma habilidade adicionada ainda.</p>)}
             </div>
 
+            <div className="flex">
+                <Button label="Adicionar Habilidade" icon={<FaPlus />} tooltip="Adicionar Habilidade" onClick={handleAdd} />
+            </div>
         </div>
     )
 }
